662eec771c3e03037865735e9447af608c694ab1
oda <odaneo@outlook.com>
2025-02-13 21:56:28 +0900

    chore: update version v0.1.2

--- packages/storage/lib/impl/temperatureStorage.ts
+++ packages/storage/lib/impl/temperatureStorage.ts
@@ -1,7 +1,7 @@
 import { StorageEnum } from '../base/enums';
 import { createStorage } from '../base/base';
 
-export const temperatureStorage = createStorage<number>('temperature-key', 0.7, {
+export const temperatureStorage = createStorage<number>('temperature-key', 1, {
   storageEnum: StorageEnum.Local,
   liveUpdate: false,
 });
