da3bf373b2c10db9465ac2bcbb542f0ee0a0c194
oda <odaneo@outlook.com>
2025-03-17 02:00:08 +0900

    fix: resize

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -147,7 +147,7 @@ export const Popup = () => {
 
   return (
     <div
-      className="w-[500px] h-[400px] p-2 flex flex-col space-y-2 text-sm transition-colors duration-300 
+      className="w-[400px] h-[500px] p-2 flex flex-col space-y-2 text-sm transition-colors duration-300 
     dark:bg-slate-900 dark:text-slate-100 bg-slate-50 text-slate-900"
     >
       <Header />
